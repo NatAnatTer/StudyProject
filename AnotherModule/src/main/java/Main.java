29a7dc1b5a205ee0a47ba92747cb273fbb8f2acb
Natalia <natinho@yandex.ru>
2024-10-22 10:47:23 +0300

    first commit

--- AnotherModule/src/main/java/Main.java
+++ AnotherModule/src/main/java/Main.java
@@ -3,4 +3,11 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hi from Another module");
     }
+
+    public static void firstMethodSP() {
+        System.out.println("This my Study project module");
+        System.out.println("first commit");
+        System.out.println("second commit");
+        System.out.println("third commit");
+    }
 }
