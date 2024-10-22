8be4629faeefe7cb77328cee27b59b8fd047da80
Natalia <natinho@yandex.ru>
2024-10-22 09:48:37 +0300

    first commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,19 @@
-//package AnotherModule3.org.example;
+
 
 
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         String ver = System.getProperty("project.version");
         System.out.println("Hi " + ver);
+        firstMethodSP();
 
+    }
 
+    public static void firstMethodSP() {
+        System.out.println("This my Study project module");
+        System.out.println("first commit");
+        System.out.println("second commit");
+        System.out.println("third commit");
     }
 }
